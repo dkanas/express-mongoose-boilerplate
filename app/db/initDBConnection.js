6a2fba377923b8916e17c4c62da5049f59fc27b0
Daniel Kanas <dkanas95@gmail.com>
2018-04-28 14:43:34 +0200

    replaced console logs with winston

--- app/db/initDBConnection.js
+++ app/db/initDBConnection.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose')
+const logger = require('../utils/logger')
 
 function initDBConnection () {
   mongoose
-    .connect(process.env.DB_URI)
-    .then(() => {
-      const { host, port } = mongoose.connection.db.serverConfig
-      const { databaseName } = mongoose.connection.db
-      console.log(`Successfully connected to DB: \n Host: ${host} \n Port: ${port} \n Database: ${databaseName}`)
-    })
-    .catch(console.error)
+  .connect(process.env.DB_URI)
+  .then(() => {
+    const { host, port } = mongoose.connection.db.serverConfig
+    const { databaseName } = mongoose.connection.db
+    logger.info(`Successfully connected to DB: \n Host: ${host} \n Port: ${port} \n Database: ${databaseName}`)
+  })
+  .catch(err => logger.error(err))
 }
 
 module.exports = initDBConnection
