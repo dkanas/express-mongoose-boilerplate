acc4d082c1ca722f13fedcea2f1d30a032ebd914
Daniel Kanas <dkanas95@gmail.com>
2018-04-28 14:32:45 +0200

    added whitespace

--- app/server.js
+++ app/server.js
@@ -8,11 +8,13 @@ const logger = require('./utils/logger')
 function startServer () {
   initDBConnection()
   const app = express()
+
   app.use(bodyParser.json())
   app.use(requestLogger)
   app.use(errorLogger)
   app.use(router)
   app.use(errorHandler)
+
   app.listen(process.env.PORT, () => logger.info(`App started on port ${process.env.PORT}!`))
 }
 
